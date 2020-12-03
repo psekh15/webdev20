d8b90940fe0555471798dbd3afaa5eb3a41f61f9
psekh15 <sk.manpreet@gmail.com>
2020-12-02 22:55:43 -0800

    mod 9 solution

---
+++ mod9-solution/src/category.controller.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('CategoryController', CategoryController);
+
+
+CategoryController.$inject = ['items'];
+function CategoryController(items) {
+  var categoryCtrl = this;
+  categoryCtrl.items = items;
+}
+
+})();
---
+++ mod9-solution/src/routes.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'templates/home.template.html'
+  })
+  .state('categoryList', {
+    url: '/category-list',
+    templateUrl: 'templates/category.template.html',
+    controller: 'CategoryController as categoryCtrl',
+    resolve: {
+      items: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+  .state('items', {
+    url: '/items/{category}',
+    templateUrl: 'templates/items.template.html',
+    controller: 'ItemsController as itemsCtrl',
+    resolve: {
+      items: ['MenuDataService', '$stateParams', function (MenuDataService, $stateParams) {
+        return MenuDataService.getItemsForCategory($stateParams.category);
+      }]
+    }
+  });
+
+
+}
+
+})();
